6fbb54a44dfb05b9fcbefabafd80708a60faeb64
Clint Morgan <crypto@makingthebest.me>
2023-07-16 12:34:58 -0400

    Lots of fixes

--- src/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="omega_bot",
-    version="0.1",
-    packages=["omega_bot"],
-    install_requires=["kucoin", "pandas", "pyalgotrade", "sklearn", "tensorflow", "joblib", "apscheduler"]
-)
